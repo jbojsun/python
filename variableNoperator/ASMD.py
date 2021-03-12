b32b7a74c6499a1d9dd51bdbc7a68f60d5165919
jbojsun <80086085+jbojsun@users.noreply.github.com>
2021-03-12 16:04:32 +0900

    Add files via upload

---
+++ variableNoperator/ASMD.py
@@ -0,0 +1,17 @@
+#두 정수를 입력받아 사칙연산 결과를 출력
+a = int(input("첫번째 숫자 :"))
+b = int(input("두번째 숫자 :"))
+print(a,'+',b,'=',a+b)
+print(a,'-',b,'=',a-b)
+print(a,'*',b,'=',a*b)
+print(a,'/',b,'=',a/b)
+
+#블럭 주석
+'''a = int(input("첫번째 숫자 :"))
+b = int(input("두번째 숫자 :"))
+print(a,'+',b,'=',a+b)
+print(a,'-',b,'=',a-b)
+print(a,'*',b,'=',a*b)
+print(a,'/',b,'=',a/b)'''
+
+
---
+++ variableNoperator/strNint.py
@@ -0,0 +1,11 @@
+#문자열 입력
+a = input(" 첫번째 숫자:")
+b = input(" 두번째 숫자:")
+print(a+b)
+print("hello"+"python")
+
+#문자열을 숫자로 변환시키는 함수 int
+
+a = int(input(" 첫번째 숫자:"))
+b = int(input(" 두번째 숫자:"))
+print(a+b)

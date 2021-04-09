83e0723777bf61484113818d324a8674885ee0ee
jbojsun <80086085+jbojsun@users.noreply.github.com>
2021-04-09 15:48:04 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,11 @@
+fstName = ['K','I','M']
+sndName = ["Jong","Bo"]
+stuNum = [2,0,2,1,0,6,0,3,3]
+
+print(fstName)
+print(sndName)
+#화면에 K, I, B 을 출력하는 프로그램을 작성하시오.
+print(fstName[0])
+print(fstName[1][0])
+print(sndName[1][0])
+

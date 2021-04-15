ae4e42ff8ca37219d546dc6e0ca2a6c9e0f39f15
jbojsun <80086085+jbojsun@users.noreply.github.com>
2021-04-15 16:57:09 +0900

    Add files via upload

---
+++ 0415/ifStmt.py
@@ -0,0 +1,15 @@
+#filename : ifStmt.py
+
+# ifStmt - if statement
+# 1. if 조건문
+# 2. if 조건문 else
+# 3. if 조건문 elif elif elif elif 조건문... else
+
+
+num = int(input("나이를 입력하시오:"))
+
+money = 10000
+if num > 20:
+    money += 5000 #money = money+5000
+
+print("당신의 입장료는",money,"입니다.")
